35a06972870b3ff10279c3e998dfb58aa7621761
ch-pereira <christofer.pereira.santos2403@gmail.com>
2022-10-18 21:16:16 -0300

    first commit

---
+++ src/main/java/Spring/Chris/ChrisApplication.java
@@ -0,0 +1,13 @@
+package Spring.Chris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Spring/Chris/Repository.java
@@ -0,0 +1,7 @@
+package Spring.Chris;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<Cliente, Long> {
+
+}
